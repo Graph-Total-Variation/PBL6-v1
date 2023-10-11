ceaee5fb105d43dce8534494717af668ac817635
HuyHoang27 <dhhdut278@gmail.com>
2023-10-11 16:15:10 +0700

    first

--- deepgtv/test_gtv.py
+++ deepgtv/test_gtv.py
@@ -130,7 +130,7 @@ def denoise(
         d = np.expand_dims(d, axis=2)
         psnr2 = cv2.PSNR(tref, d)
         logger.info("PSNR: {:.5f}".format(psnr2))
-        (score, diff) = compare_ssim(tref, d, full=True, multichannel=False)
+        (score, diff) = compare_ssim(tref, d, full=True, multichannel=True)
         logger.info("SSIM: {:.5f}".format(score))
     logger.info("Saved {0}".format(opath))
     if argref:
@@ -228,7 +228,7 @@ def main_eva(
 
         img1 = cv2.imread(inp)[:, :, : opt.channels]
         img2 = cv2.imread(argref)[:, :, : opt.channels]
-        (score, diff) = compare_ssim(img1, img2, full=True, multichannel=False)
+        (score, diff) = compare_ssim(img1, img2, full=True, multichannel=True)
         logger.info("Original {0:.2f} {1:.2f}".format(cv2.PSNR(img1, img2), score))
     logger.info("========================")
     # logger.info("MEAN PSNR: {:.2f}".format(np.mean(traineva["psnr"])))
@@ -278,7 +278,7 @@ def main_eva(
 
         img1 = cv2.imread(inp)[:, :, : opt.channels]
         img2 = cv2.imread(argref)[:, :, : opt.channels]
-        (score, diff) = compare_ssim(img1, img2, full=True, multichannel=False)
+        (score, diff) = compare_ssim(img1, img2, full=True, multichannel=True)
         logger.info("Original {0:.2f} {1:.2f}".format(cv2.PSNR(img1, img2), score))
     logger.info("========================")
     # logger.info("MEAN PSNR: {:.2f}".format(np.mean(testeva["psnr"])))
