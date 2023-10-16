925d50c26a7a4006c000411795f0ca80954dc969
Chonn18 <duongvanchon18@gmail.com>
2023-10-16 17:24:53 +0700

    15th commit

--- deepgtv/dgtv/my_dgtv.py
+++ deepgtv/dgtv/my_dgtv.py
@@ -404,11 +404,11 @@ class GTV(nn.Module):
             ** 2
         )
 
-        w = torch.exp(-(Fs.sum(axis=1)) / (s ** 2))
-        # if s:
-        #     w = torch.exp(-((Fs.sum(axis=1))**2) / (s ** 2))
-        # else:
-        #     w = 0
+        # w = torch.exp(-(Fs.sum(axis=1)) / (s ** 2))
+        if s:
+            w = torch.exp(-((Fs.sum(axis=1))**2) / (s ** 2))
+        else:
+            w = 0
         if debug:
             s = f"Sample WEIGHT SUM: {w[0, :, :].sum().item():.4f} || Mean Processed u: {u.mean().item():.4f}"
             self.logger.info(s)
@@ -540,11 +540,11 @@ class GTV(nn.Module):
                 E.view(E.shape[0], E.shape[1], self.opt.width ** 2, 1))
             ** 2
         )
-        # if self.weight_sigma:
-        #     w = torch.exp(-((Fs.sum(axis=1))**2) / (self.weight_sigma ** 2))
-        # else:
-        #     w = 0
-        w = torch.exp(-(Fs.sum(axis=1)) / (self.weight_sigma ** 2))
+        if self.weight_sigma:
+            w = torch.exp(-((Fs.sum(axis=1))**2) / (self.weight_sigma ** 2))
+        else:
+            w = 0
+        # w = torch.exp(-(Fs.sum(axis=1)) / (self.weight_sigma ** 2))
 
         if manual_debug:
             # return_dict['gtv'].append((z*w).abs().sum())
